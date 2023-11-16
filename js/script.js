5a2e34a9489e1ff0354e88860fafdba9f4fda851
devanomrz <142818097+devanomrz@users.noreply.github.com>
2023-11-16 14:35:39 +0700

    perbaikan sedikit bug

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ document.addEventListener("click", function (e) {
 // script.js
 
 function kirimPesan() {
+  console.log("Tombol diklik");
   // Mengambil nilai dari setiap elemen formulir
   var nama = document.getElementById("nama").value;
   var alamatEmail = document.getElementById("alamatEmail").value;
@@ -29,11 +30,15 @@ function kirimPesan() {
   var pesanWhatsApp =
     "Halo, saya ingin memesan telur gulung dengan detail berikut:%0a";
   pesanWhatsApp += "Nama: " + nama + "%0a";
-  pesanWhatsApp += "Alamat Email: " + alamatEmail + "%0a";
+  pesanWhatsApp += "Alamat: " + alamat + "%0a";
   pesanWhatsApp += "No Hp: " + noHp + "%0a";
   pesanWhatsApp += "Pesan: " + pesan;
 
   // Mengarahkan pengguna ke tautan WhatsApp dengan pesan yang dihasilkan
+  console.log(
+    "URL WhatsApp: ",
+    "https://wa.me/628198885434?text=" + encodeURIComponent(pesanWhatsApp)
+  );
   window.location.href =
     "https://wa.me/628198885434?text=" + encodeURIComponent(pesanWhatsApp);
 }
