bcee7d7861fd308a58c73ea832a1e8b4ef2288c4
devanomrz <142818097+devanomrz@users.noreply.github.com>
2023-11-16 14:54:48 +0700

    perubahan sedikit

--- js/script.js
+++ js/script.js
@@ -23,15 +23,15 @@ function kirimPesan() {
   // Mengambil nilai dari setiap elemen formulir
   var nama = document.getElementById("nama").value;
   var alamat = document.getElementById("alamat").value;
-  var noHp = document.getElementById("noHp").value;
+  // var noHp = document.getElementById("noHp").value;
   var pesan = document.getElementById("pesan").value;
 
   // Membuat pesan WhatsApp dengan menggabungkan data formulir
   var pesanWhatsApp =
-    "Halo, saya ingin memesan telur gulung dengan detail berikut:%0a";
-  pesanWhatsApp += "Nama: " + nama + "%0a";
-  pesanWhatsApp += "Alamat: " + alamat + "%0a";
-  pesanWhatsApp += "No Hp: " + noHp + "%0a";
+    "Halo, saya ingin memesan telur gulung dengan detail berikut:\n";
+  pesanWhatsApp += "Nama: " + nama + "\n";
+  pesanWhatsApp += "Alamat: " + alamat + "\n";
+  // pesanWhatsApp += "No Hp: " + noHp + "\n";
   pesanWhatsApp += "Pesan: " + pesan;
 
   // Mengarahkan pengguna ke tautan WhatsApp dengan pesan yang dihasilkan
