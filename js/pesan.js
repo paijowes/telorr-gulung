d5cf9ad95606d7dba5a2a613144eb651c96e3a2b
devanomrz <142818097+devanomrz@users.noreply.github.com>
2023-11-16 14:29:08 +0700

    untuk pesan

--- js/pesan.js
+++ js/pesan.js
@@ -1,21 +1,3 @@
-// Toogle class active
-const navbarNav = document.querySelector(".navbar-nav");
-
-// ketika hamburger menu
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
-
-// Klik di luar sidebar untuk menghilangkan hamburger menu
-
-const hamburger = document.querySelector("#hamburger-menu");
-
-document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
-  }
-});
-
 // script.js
 
 function kirimPesan() {
